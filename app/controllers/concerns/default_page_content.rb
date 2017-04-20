4b6fa3a3690f9e6566547ebe591ba56c54da48d7
Trevor <trevorhoward2@hotmail.com>
2017-04-20 14:05:38 -0600

    Added nav bar and added default header content

---
+++ app/controllers/concerns/default_page_content.rb
@@ -0,0 +1,12 @@
+module DefaultPageContent
+	extend ActiveSupport::Concern
+
+	included do
+		before_filter  :set_page_defaults
+	end
+	
+  def set_page_defaults
+  	@page_title = "My Portfolio | My Portfolio Website"
+  	@seo_keywords = "Trevor Howard Portfolio"
+  end
+end
