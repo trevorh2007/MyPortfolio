87bbcfb0a2b722275f0f604b580d353e14c77028
Trevor <trevorhoward2@hotmail.com>
2017-04-20 15:02:43 -0600

    Implemented content tag helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+
 	def login_helper
 		if current_user.is_a?(User)
     	link_to "Logout", destroy_user_session_path, method: :delete
@@ -8,4 +9,12 @@ module ApplicationHelper
       (link_to "Login", new_user_session_path)
     end 
   end
+
+  def source_helper(layout_name)
+  	if session[:source]
+  		greeting = "Thanks for visiting me from #{session[:source].capitalize}! You're on the #{layout_name} layout!"
+  		content_tag(:p, greeting, class: "source-greeting")
+    end
+  end
+
 end
