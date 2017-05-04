b9ce5cc7ef089006c71fe24b5928abd92449dd3d
Trevor <trevorhoward2@hotmail.com>
2017-05-04 16:48:17 -0600

    Added radio buttons to blog edit page for draft and published

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -91,6 +91,6 @@ class BlogsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
-      params.require(:blog).permit(:title, :body, :notes)
+      params.require(:blog).permit(:title, :body, :notes, :status)
     end
 end
