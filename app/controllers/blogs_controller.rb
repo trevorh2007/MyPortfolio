5a85fb94a88573755a68ca710aa1e155f3cf9c62
Trevor <trevorhoward2@hotmail.com>
2017-05-06 23:19:25 -0600

    Added validations for topics in blogs and added from element

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -91,6 +91,6 @@ class BlogsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
-      params.require(:blog).permit(:title, :body, :notes, :status)
+      params.require(:blog).permit(:title, :body, :notes, :status, :topic_id)
     end
 end
