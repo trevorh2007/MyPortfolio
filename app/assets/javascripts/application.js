4de6d6db2b7aea9eafa8e9877c7b455ee4621403
Trevor <trevorhoward2@hotmail.com>
2017-05-08 16:43:21 -0600

    Fixed JS loading bug

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,6 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap-sprockets
 //= require jquery_ujs
 //= require jquery-ui
 //= require html.sortable
@@ -21,3 +20,4 @@
 //= require cable
 //= require turbolinks
 //= require_tree .
+//= require bootstrap-sprockets
