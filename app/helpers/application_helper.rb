64f66c80fb7c1488681bd95e739a7e4177257360
Trevor <trevorhoward2@hotmail.com>
2017-05-08 17:39:25 -0600

    Fixed logout bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
       " ".html_safe +
       (link_to "Login", new_user_session_path, class: style)
     else
-      link_to "Logout", destroy_user_session_path, :method => :delete, class: style
+      link_to "Logout", destroy_user_session_path(:user), :method => :delete, class: style
     end 
   end
 
