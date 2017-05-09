4e9d5fbf3146aa0b2e1a6359f2b159409ddaaff2
Trevor <trevorhoward2@hotmail.com>
2017-05-08 18:07:21 -0600

    Integrated production calls for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://trevorhoward.herokuapp.com', 'http://trevorhoward.herokuapp.com']
+  config.action_cable.url = "wss://trevorhoward.herokuapp.com/cable"
 end
